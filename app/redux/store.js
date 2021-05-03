23c0b4c99d8d9d0426f3920aa6862b476a805f32
safad <safadeldoul@gmail.com>
2021-05-03 23:52:48 +0100

    added posts and comments

--- app/redux/store.js
+++ app/redux/store.js
@@ -4,12 +4,15 @@ import createSagaMiddleware from "redux-saga";
 
 import reducers from "./reducers";
 import rootSaga from "./sagas";
-
+import {Post_Reducer} from './posts/reducer'
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   combineReducers({
     ...reducers,
+    Post_Reducer:Post_Reducer
+
+
     // router: routerReducer
   }), 
   applyMiddleware(sagaMiddleware)
